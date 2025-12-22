76d63de2364881fba8b17d63e8d328c3e39075e3
Bamu <a892842486@github.com>
2025-12-22 10:35:40 +0800

    feat(admin/products): 瀏覽商品頁面與動作

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -5,12 +5,16 @@ class Admin::ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    
+
     if @product.save
       redirect_to admin_products_path
     else
       render :new
     end
+  end
+
+  def index
+    @products = Product.all
   end
 
   private
